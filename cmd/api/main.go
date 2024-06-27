5667aa2f92fd06ace3642ddf1109598f8dadc182
Paul Cockrell <paulcockrell@gmail.com>
2024-06-27 08:20:40 +0100

    Add DB connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
 
 type config struct {
 	env  string
+	db   struct{ dsn string }
 	port int
 }
 
@@ -28,10 +33,23 @@ func main() {
 	// Read the value of the port and env command-line flags into the config struct.
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
 	flag.Parse()
 
 	// Initialize a new logger
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// Create DB connection pool
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	// Defer call to db.Close() so that the connection pool is close before the main()
+	// function exits
+	defer db.Close()
+
+	logger.Printf("database connection pool established")
 
 	// Declare an instance of the application struct
 	app := &application{
@@ -50,6 +68,27 @@ func main() {
 
 	// Start the HTTP server
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Use PingContext to establish a new connection to the database, passing in the
+	// context we created above as a parameter. If the connection couldn't be
+	// established successfsully withing the 5 second deadline, the n this will return an
+	// error.
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
