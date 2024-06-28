049282b8165083b8d1952701b6ad0048a9245244
Paul Cockrell <paulcockrell@gmail.com>
2024-06-28 15:10:24 +0100

    Add more database configuration parameters
    So we can fine tune the database connection pool, accept three more variables to setup connections

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -16,8 +16,13 @@ import (
 const version = "1.0.0"
 
 type config struct {
-	env  string
-	db   struct{ dsn string }
+	env string
+	db  struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
 	port int
 }
 
@@ -30,10 +35,16 @@ func main() {
 	// Declare instance of the config struct
 	var cfg config
 
-	// Read the value of the port and env command-line flags into the config struct.
+	// Read the value of the port and env command-line flags into the config struct
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+
+	// Read database configurations settings into the config struct
 	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle connections")
+
 	flag.Parse()
 
 	// Initialize a new logger
@@ -77,6 +88,15 @@ func openDB(cfg config) (*sql.DB, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetConnMaxIdleTime(duration)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
