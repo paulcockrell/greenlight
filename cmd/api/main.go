674615f881100d503adcb09947fadd25034f5b0d
Paul Cockrell <paulcockrell@gmail.com>
2024-08-17 09:19:12 +0100

    Add background emailer job
    Add background jobs for email tasks, introduce wait groups so shutdowns
    perform gracefully with regard to background processes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"sync"
 	"time"
 
 	_ "github.com/lib/pq"
@@ -43,6 +44,7 @@ type application struct {
 	logger *jsonlog.Logger
 	mailer mailer.Mailer
 	config config
+	wg     sync.WaitGroup
 }
 
 func main() {
